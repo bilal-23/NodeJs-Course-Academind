e5822a8ce5b1b2dcf5872cc2a3c2fb42959d55bc
Bilal <mansuribilal101@gmail.com>
2021-12-14 17:46:17 +0530

    Working with res and req

--- 3-Understanding Basics/app.js
+++ 3-Understanding Basics/app.js
@@ -1,11 +1,38 @@
 //using http module
 const http = require('http');
+const fs = require('fs');
 
 function rqListener(req, res) {
     // req have data about request
     //res will help to send response
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if (url === "/") {
+        res.write('<html>');
+        res.write('<title>First Page</title>')
+        res.write(`<body> <form action="/message" method="POST"> 
+        <input type="text" id="msg" name="message"/>
+        <label htmlFor="msg">Add message</label>
+        <button type="submit">Submit</button>
+        </form> </body>`)
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end();
+
+        // const message = req
+        // console.log(message);
+        res.write('<html>');
+        res.write('<title>First Page</title>')
+        res.write(`<body> <h1>${"hello"}</h1> </body>`)
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<title>First Page</title>')
