e035b421bd6e7dc401139d08066a055f0c0346d8
Bilal <mansuribilal101@gmail.com>
2021-12-14 17:33:23 +0530

    Request and Response

--- 3-Understanding Basics/app.js
+++ 3-Understanding Basics/app.js
@@ -4,7 +4,14 @@ const http = require('http');
 function rqListener(req, res) {
     // req have data about request
     //res will help to send response
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<title>First Page</title>')
+    res.write('<body> <h1>Hello from my Nodejs Server</h1> </body>')
+    res.write('</html>');
+    res.end();
 }
 const server = http.createServer(rqListener); //it takes a function 
 
