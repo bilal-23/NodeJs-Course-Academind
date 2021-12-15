686391b60a7b66c1cb0832f303cdaae2ba738ab8
Bilal <mansuribilal101@gmail.com>
2021-12-15 16:00:53 +0530

    Parsing Request Bodies

--- 3-Understanding Basics/app.js
+++ 3-Understanding Basics/app.js
@@ -19,7 +19,18 @@ function rqListener(req, res) {
         return res.end();
     }
     if (url === "/message" && method === "POST") {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
         res.statusCode = 302;
         res.setHeader('Location', '/')
         return res.end();
