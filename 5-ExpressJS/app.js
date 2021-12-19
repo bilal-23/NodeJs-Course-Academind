0a0a0bae29a85feef2b0337f6bfbc244a9d0ccb1
Bilal <mansuribilal101@gmail.com>
2021-12-19 17:33:33 +0530

    Serving html files

--- 5-ExpressJS/app.js
+++ 5-ExpressJS/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -12,7 +13,9 @@ app.use(shopRoute);
 
 app.use((req, res, next) => {
     res.status(404);
-    res.send('<h1>Page not found</h1>');
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
+
+// module.exports = path.dirname(require.main.filename);
--- 5-ExpressJS/routes/admin.js
+++ 5-ExpressJS/routes/admin.js
@@ -1,13 +1,11 @@
+const path = require('path');
 const express = require("express");
 const router = express.Router();
 
 
 router.get('/add-product', (req, res) => {
     console.log('Adding new product');
-    res.send(`<form action="/admin/add-product" method="Post">
-    <input type="text" name="title"/>
-    <button type="submit">Add product</button>
-    </form>`)
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
