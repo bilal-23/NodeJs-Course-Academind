15e456f1f717033842135823120a61b69bc41fdc
Bilal <mansuribilal101@gmail.com>
2021-12-19 16:26:19 +0530

    parsing incoming request

--- 5-ExpressJS/app.js
+++ 5-ExpressJS/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Inside the middleware')
-    next(); //Allow to request to continue to the next middleware below this middleware in line.
+app.use(bodyParser.urlencoded({ extended: false })); //it'll parse body
+
+app.use('/add-product', (req, res) => {
+    console.log('Adding new product');
+    res.send(`<form action="/product" method="Post">
+    <input type="text" name="title"/>
+    <button type="submit">Add product</button>
+    </form>`)
 });
-app.use((req, res, next) => {
-    console.log('Inside another middleware');
-    //Headers is automatically setby express
-    res.send('<h1>Hello From Express</h1>')
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-//use allow us to add middleware function
-//next is a function passed to the arrow function which will be exectured after the arrow function
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello From Express</h1>')
+});
 
 app.listen(3000);
