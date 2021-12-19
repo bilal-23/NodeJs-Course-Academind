bdb54cc04463d2e91e33f3c617bd0a1b2b55aa07
Bilal <mansuribilal101@gmail.com>
2021-12-19 16:47:58 +0530

    Using Express Router

--- 5-ExpressJS/app.js
+++ 5-ExpressJS/app.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoute = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false })); //it'll parse body
 
-app.use('/add-product', (req, res) => {
-    console.log('Adding new product');
-    res.send(`<form action="/product" method="Post">
-    <input type="text" name="title"/>
-    <button type="submit">Add product</button>
-    </form>`)
-});
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(adminRoutes);
+app.use(shopRoute);
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express</h1>')
-});
+
 
 app.listen(3000);
---
+++ 5-ExpressJS/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get('/add-product', (req, res) => {
+    console.log('Adding new product');
+    res.send(`<form action="/product" method="Post">
+    <input type="text" name="title"/>
+    <button type="submit">Add product</button>
+    </form>`)
+});
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
