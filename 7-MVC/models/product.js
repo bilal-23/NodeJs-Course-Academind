357d77f39f98dea09868772e8cf49e08b1d966c0
Bilal <mansuribilal101@gmail.com>
2021-12-28 16:47:13 +0530

    Refactoring product model

--- 7-MVC/models/product.js
+++ 7-MVC/models/product.js
@@ -2,31 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
 
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     save() {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-
-        });
-    }
-    static fecthAll(cb) {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
         })
+    }
+
+    static fecthAll(cb) {
+        getProductsFromFile(cb);
     }
 }
