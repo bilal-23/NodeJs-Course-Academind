cd05b11fcd781fbed52678c2605505ce12e79fee
Bilal <mansuribilal101@gmail.com>
2021-12-28 16:40:21 +0530

    Added Product Model

--- 7-MVC/controllers/products.js
+++ 7-MVC/controllers/products.js
@@ -1,14 +1,18 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { docTitle: 'Add Product', path: '/admin/add-product' })
 }
 
 exports.addNewProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', { prods: products, docTitle: 'SHOP', path: '/', hasProducts: products.length > 0 });
+    Product.fecthAll((products) => {
+        res.render('shop', { prods: products, docTitle: 'SHOP', path: '/', hasProducts: products.length > 0 });
+    });
+
 }
