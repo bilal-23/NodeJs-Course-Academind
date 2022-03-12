7dd4ddbc4c28294e33e4637b9ee0d2585c57d765
Bilal <mansuribilal101@gmail.com>
2022-03-12 21:17:03 +0530

    init commit sequelize

---
+++ 11-Sequelize/controllers/admin.js
@@ -0,0 +1,75 @@
+const Product = require('../models/product');
+
+
+//All products list
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+
+    });
+  });
+};
+
+//Add product form page
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+
+  });
+};
+
+//Add product endpoint
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+//Edit Product page
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (editMode === "false") {
+    res.redirect('/admin/products')
+  }
+
+  const productId = (req.params.productId);
+  Product.findById(productId, (product) => {
+    res.render('admin/edit-product', {
+      product: product,
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+    })
+  })
+}
+
+//Edit Product form endpoint
+exports.postEditProduct = (req, res) => {
+  const { productId, title, imageUrl, price, description } = req.body;
+  const product = new Product(productId, title, imageUrl, description, price);
+  // create new instance 
+  product.save();
+  //overwriting on the already saved data
+  res.redirect('/admin/products')
+}
+
+//delete product form endpoint
+exports.postDeleteProduct = (req, res) => {
+  const productId = req.body.productId;
+  //calling method from Product class to delete this product;
+  Product.deleteProduct(productId);
+  //redirect to admin products list
+  res.redirect('/admin/products')
+}
---
+++ 11-Sequelize/controllers/shop.js
@@ -0,0 +1,98 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll().then(([rows, fieldData]) => {
+    res.render('shop/product-list', {
+      prods: rows,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  })
+
+    .catch(err => console.log(err));
+};
+
+exports.getProductDetail = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId).then(([product]) => {
+    res.render('shop/product-detail', {
+      product: product[0],
+      pageTitle: product[0].title,
+      path: '/products'
+    })
+  });
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll().then(([rows, fieldData]) => {
+    res.render('shop/index', {
+      prods: rows,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+
+    .catch(err => console.log(err));
+  // Product.fetchAll(products => {
+  //   res.render('shop/index', {
+  //     prods: products,
+  //     pageTitle: 'Shop',
+  //     path: '/'
+  //   });
+  // });
+};
+
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    const totalPrice = cart.totalPrice;
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      products.forEach(product => {
+        const cartProductData = cart.products.find(item => item.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ ...product, quantity: cartProductData.quantity });
+        }
+      })
+      console.log(cartProducts);
+      res.render('shop/cart', {
+        path: '/cart',
+        products: cartProducts,
+        totalPrice: totalPrice,
+        pageTitle: 'Your Cart'
+      })
+    })
+  });
+};
+
+//deltew item from cart
+exports.postDeleteCartItem = (req, res) => {
+  const id = req.body.productId;
+  Product.findById(id, product => {
+    Cart.deleteProduct(id, product.price);
+    res.redirect('/cart');
+  })
+}
+
+//adds item to the cart for url add-to-cart
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId.trim();
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, +product.price)
+  })
+  res.redirect('/cart')
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
