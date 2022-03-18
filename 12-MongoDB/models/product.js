c922f1ffaaaa16750c52ac1557bcdc3e0b304ac5
Bilal <mansuribilal101@gmail.com>
2022-03-18 16:33:46 +0530

    connecting to the database

---
+++ 12-MongoDB/models/product.js
@@ -0,0 +1,15 @@
+const getDB = require('./util/database').getDB;
+
+class Product {
+    constructor(title, price, description, imageUrl) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+    save(){
+        
+    }
+}
+
+module.exports = Product;
--- 12-MongoDB/util/database.js
+++ 12-MongoDB/util/database.js
@@ -2,17 +2,29 @@ const res = require('express/lib/response');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
 
 const mongoConnect = (callback) =>{   
     MongoClient
     .connect(`mongodb+srv://bilal:bilal123@cluster0.tfip8.mongodb.net/nodejscourse?retryWrites=true&w=majority`)
     .then(client =>{
         console.log(`Connected to MongoDB`);
-        callback(client);
+        _db = client.db(); //this will connect to db mentioned in the above link which is nodejscourse
+        callback();
     })
     .catch(err =>{
         console.log(`Error: ${err}`);
+        throw new Error(err);
     })
 }
-    
-module.exports = mongoConnect;
+
+//get db will give back a db connected to database
+const getDb = () =>{
+    if(_db){
+        return db;
+    }
+    throw new Error(`No database found`);
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
